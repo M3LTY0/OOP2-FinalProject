88fb77daa2874f30cd254aaf3e78305e4640f3c2
MScarrott <scarrottmatthew@gmail.com>
2026-04-15 10:25:11 -0600

    Started test classes

--- tests/testing/BookTest.java
+++ tests/testing/BookTest.java
@@ -1,5 +1,44 @@
 package tests.testing;
 
-public class BookTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.*;
+
+public class BookTest {
+    private BookManager BM;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void testAddBook() {
+
+    }
+
+    @Test
+    void testDisplayBooks() {
+
+    }
+
+    @Test
+    void testSearchBooks() {
+
+    }
+
+    @Test
+    void testUpdateBook() {
+
+    }
+
+    @Test
+    void testDeleteBook() {
+
+    }
 }
